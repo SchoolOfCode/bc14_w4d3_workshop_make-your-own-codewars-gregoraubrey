3e36e547d75342b57e696e60b533dc6700305427
gregor <gregor.aubrey.uk@gmail.com>
2023-04-10 16:00:50 +0100

    wrote title and description for my code wars kata

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 /* 
 👉 Write your kata here!
+
+Title: STRINGS-TO-SCREAMING-KEBAB-CASE
+Description:
+
+Your task is to create a function that takes in one or more strings as an argument and returns a single string comprised of all the arguments (in input order) glued together.
+
+The string your function returns should be in SCREAMING-KEBAB-CASE, with hypens, spaces, and underscores in the input strings all marking the start of a new word.
+
+Each argument should be separated from the next by a single hyphen in the output string.
+
+If the arguments are not strings, they should be converted to strings before being concatenated.
+
+For example:
+
+ToScreamingKebab("one_two three-four") should return "ONE-TWO-THREE-FOUR".
+ToScreamingKebab("FiVE-sIx_seVen EIGHT") should return "FIVE-SIX-SEVEN-EIGHT"
+ToScreamingKebab("NINE", "Te-N", "ELE VEN", 12) should return "NINE-TE-N-ELE-VEN-12"
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function ToScreamingKebab(...str) {
+    // Your code here
+  }
