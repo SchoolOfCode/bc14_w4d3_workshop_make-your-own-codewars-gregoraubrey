a8154ef3d7bf699b6fa7aefc3a614f066aa233f5
gregor <gregor.aubrey.uk@gmail.com>
2023-04-10 17:22:32 +0100

    fixed typos in main.js description

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ Description:
 
 Your task is to create a function that takes in one or more arguments and returns a single string comprised of all the arguments (in input order) glued together.
 
-The string your function returns should be in SCREAMING-KEBAB-CASE, with hypens, spaces, and underscores in the input strings all marking the start of a new word.
+The string your function returns should be in SCREAMING-KEBAB-CASE, with any hyphens, spaces, and underscores in the input argument(s) all marking the start of a new word.
 
 Each argument should be separated from the next by a single hyphen in the output string.
 
-If any of the arguments are not strings, they should be converted to strings before being glued together.
+If any of the arguments are not strings, they should be converted to strings before being glued to the other arguments.
 
 If any of the arguments contain multiple spaces, hyphens or underscores in a row, these should all be replaced with a single hyphen in each case.
 
